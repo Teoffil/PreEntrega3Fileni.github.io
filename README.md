1fe22d6bc1fcdfa61c35106f0dba251499f73a11
Mateo Fileni <mateofileni@iMac-de-Mateo.local>
2023-03-02 16:19:21 -0300

    tercera entrega lista

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyecto Coderhouse Ecommerce Informático
+
+- El usuario podrá visualizar una lista de productos y ordenarlos según su precio. El usuario podrá visualizar una lista de productos y ordenarlos según su
+- El usuario podrá comprar productos.
+- Los productos se podrán agregar a un carrito de compras.
+- El usuario podrá visualizar los productos del carrito de compras.
+- El usuario podrá eliminar productos del carrito de compras. - EL usuario bocra eliminar Drocucios de carrito de combras
+- Se le mostrará al usuario la cantidad total y el precio total de productos en el carrito de compras.
+- El usuario podrá finalizar la compra.
+
+# Realizado por
+
+Fileni, Mateo.
---
+++ data/stock.js
@@ -0,0 +1,63 @@
+const productos = 
+[
+    {
+        id: 1,
+        nombre: 'Motherboard',
+        tipo: 'Motherboard',
+        descripcion: 'Soporta procesadores desktop 1ra, 2da y 3ra Gen AMD Ryzen™ / Ryzen™ con gráficos Radeon™ Vega y 2da Gen AMD Ryzen™ con gráficos Radeon™ / Athlon™ con gráficos Radeon™ Vega para Socket AM4',
+        precio: 24000,
+        cantidad: 0,
+        imagen: './public/images/mother.webp',
+    },
+
+    {
+        id: 2,
+        nombre: 'CPU',
+        tipo: 'Procesador',
+        descripcion: 'En este producto, encontrarás los núcleos, que son los encargados de ejecutar las instrucciones y actividades que le asignás a tu dispositivo. Estos tienen relación directa con dos elementos: los hilos y el modelo. Por lo tanto, a la hora de elegir un procesador, es importante que valores los tres en su conjunto.',
+        precio: 55000,
+        cantidad: 0,
+        imagen: './public/images/cpu.webp',
+    },
+
+    {
+        id: 3,
+        nombre: 'RAM',
+        tipo: 'Memoria RAM',
+        descripcion: 'Con su tecnología DDR4, mejorará el desempeño de tu equipo, ya que aumentará la fluidez y la velocidad en la transferencia de datos ¡Optimizá al máximo el rendimiento de tu computadora y reducí el consumo energético!',
+        precio: 20000,
+        cantidad: 0,
+        imagen: './public/images/ram.jpeg',
+    },
+
+    {
+        id: 4,
+        nombre: 'GPU',
+        tipo: 'Placa de video',
+        descripcion: 'Este componente electrónico procesa la información que llega al dispositivo y los transforma en imágenes o videos para mostrarla visualmente. Es ideal para trabajar con aplicaciones gráficas ya que permite obtener imágenes más nítidas.',
+        precio: 350000,
+        cantidad: 0,
+        imagen: './public/images/gpu.webp',
+    },
+
+    {
+        id: 5,
+        nombre: 'Fan',
+        tipo:'Ventilador',
+        descripcion:'Elaborado con las mejores tecnologías y pasión, el Radiant-D RGB Fan es una artesanía especial para modders y entusiastas de los juegos. Agrega control Digital-RGB programable con LED RGB (ARGB) direccionables individualmente que permiten configuraciones de color variables para cada uno de los 9 LED instalados en el ventilador y fomenta una experiencia de iluminación RGB increíblemente vívida. Junto con el perfil de rendimiento extremo y el diseño audaz, todo eso hace que el Radiant-D sea un ingrediente perfecto para plataformas de juegos y PC personalizadas. ¡Pruébelo, encienda todos los colores de la vida dentro de su PC!',
+        precio: 3000,
+        cantidad: 0,
+        imagen: './public/images/fan.webp',
+    },
+
+    {
+        id: 6,
+        nombre: 'Monitor',
+        tipo:'Monitor',
+        descripcion:'Este monitor de 27" te va a resultar cómodo para estudiar, trabajar o ver una película en tus tiempos de ocio. Asimismo, su resolución de 2560 x 1440 te permite disfrutar de momentos únicos gracias a una imagen de alta fidelidad. Su tiempo de respuesta de un milisegundo lo hace ideal para gamers y cinéfilos porque es capaz de mostrar imágenes en movimiento sin halos o bordes borrosos.',
+        precio: 150000,
+        cantidad: 0,
+        imagen: './public/images/monitor.webp',
+    },
+
+]
